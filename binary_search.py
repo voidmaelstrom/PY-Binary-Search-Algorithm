04185100eb4a578d0977142989df2de5bfcb859c
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-06-21 22:14:24 -0600

    Activity: Binary Search Algorithm commits

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,21 @@
-# code your iterative algorithm here
+# code your iterative algorithm below here
+def binary_search(data, el):
+    first = 0
+    last = len(data)-1 
+    found = False
 
+    while first <= last and not found:
+        mid = (first+last)//2
+
+        if data[mid] == el:
+            found = True
+        else:
+            if el < data[mid]: 
+                last = mid-1
+            else: 
+                first = mid+1
+    return found
+
+test_list = [5, 8, 12, 14, 19, 22, 27, 30, 31]
+
+print(binary_search(test_list, 12))
